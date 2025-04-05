020dde95bd3d9812f0a4ff2b801ac2d332432289
unknown <sandyriswantino1933@gmail.com>
2025-04-05 11:30:05 +0700

    add seeder auth user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $adminRole = Role::create([
+            'name' => 'admin',
+            'guard_name' => 'web',
+        ]);
+
+        $penulisRole = Role::create([
+            'name' => 'penulis',
+            'guard_name' => 'web',
+        ]);
+
+        $userRole = Role::create([
+            'name' => 'user',
+            'guard_name' => 'web',
+        ]);
+
+        $admin = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'avatar' => 'images/default-avatar.png',
+        ]);
+
+        $penulis = User::factory()->create([
+            'name' => 'Penulis',
+            'email' => 'penulis@gmail.com',
+            'password' => bcrypt('12345678'),
+            'avatar' => 'avatars/default-avatar.png',
+        ]);
+
+        $user = User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('12345678'),
+            'avatar' => 'avatars/default-avatar.png',
+        ]);
+
+        $admin->assignRole($adminRole);
+        $penulis->assignRole($penulisRole);
+        $user->assignRole($userRole);
     }
 }
